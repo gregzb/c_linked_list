409a1dbd1b1c73ff5a5ebd0ea8fdb0df64ca31cf
Greg Z <greg.zbo3@gmail.com>
2019-10-27 21:00:57 -0400

    boop

--- functions.c
+++ functions.c
@@ -5,17 +5,17 @@
 
 void print_list(struct node * list) {
     struct node * current = list;
-    printf("[ ")
+    printf("[ ");
     while (current != NULL) {
         printf("%d ", current->val);
         current = current->next;
     }
-    printf("]\n")
+    printf("]\n");
 
 }
 
 struct node * insert_front(struct node * list, int val) {
-    struct node * new_node = calloc(sizeof(struct node));
+    struct node * new_node = malloc(sizeof(struct node));
     new_node->val = val;
     new_node->next = list;
     return new_node;
@@ -26,27 +26,33 @@ struct node * free_list(struct node * list) {
     struct node * next = current;
     while (current != NULL) {
         next = current->next;
+        printf("freeing %d\n", current->val);
         free(current);
         current = next;
     }
-    return list;
+    return NULL;
 }
 
 struct node * remove_node(struct node *front, int data) {
-    if (front == NULL) {
-        return front;
-    }
-    if (front->val == data) {
-        struct node *temp = front->next;
-        free(front);
-        return temp;
-    }
+
+    struct node * prev = NULL;
     struct node * current = front;
+
     while (current != NULL) {
-        if (current->next == data) {
-            free(current->next);
-            current->next = current->next->next;
+        if (current->val == data) {
+            if (prev == NULL) {
+                struct node * temp = current->next;
+                free(current);
+                return temp;
+            }
+            prev->next = current->next;
+            free(current);
+            return front;
         }
+
+        prev = current;
+        current = current -> next;
     }
+
     return front;
 }
--- main.c
+++ main.c
@@ -4,15 +4,38 @@
 #include "functions.h"
 
 int main() {
+
+    printf("making list\n");
     struct node * my_list = NULL;
-    my_list = insert_front(my_list, 4);
-    my_list = insert_front(my_list, -1234);
-    my_list = insert_front(my_list, 612);
-    my_list = insert_front(my_list, 324234);
-    my_list = insert_front(my_list, -234234);
-    my_list = insert_front(my_list, 100);
+    printf("empty list: \n");
     print_list(my_list);
-    free_list(my_list);
+
+    int i;
+    for (i = 0; i < 6; i++) {
+        my_list = insert_front(my_list, i);
+        printf("adding %i\n", i);
+        print_list(my_list);
+    }
+
+    my_list = remove_node(my_list, 0);
+    printf("removing 0: \n");
+    print_list(my_list);
+
+    my_list = remove_node(my_list, 2);
+    printf("removing 2: \n");
+    print_list(my_list);
+
+    my_list = remove_node(my_list, 5);
+    printf("removing 5: \n");
+    print_list(my_list);
+
+    my_list = remove_node(my_list, 6);
+    printf("removing 6: \n");
+    print_list(my_list);
+
+    printf("freeing my list: \n");
+    my_list = free_list(my_list);
+    printf("freed list: %p\n", my_list);
 
     return 0;
 }
